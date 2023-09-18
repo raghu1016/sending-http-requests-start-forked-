570c64fc64c6ae5b7ba6d3d01e14735d3061dd03
CodeSandbox <support@codesandbox.io>
2023-09-18 14:56:53 +0000

    Sandbox snapshot commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,76 @@
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { useRef, useState, useCallback } from 'react';
 
-import Events from "./components/Events/Events.jsx";
-import EventDetails from "./components/Events/EventDetails.jsx";
-import NewEvent from "./components/Events/NewEvent.jsx";
-import EditEvent from "./components/Events/EditEvent.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to="/events" />,
-  },
-  {
-    path: "/events",
-    element: <Events />,
-
-    children: [
-      {
-        path: "/events/new",
-        element: <NewEvent />,
-      },
-    ],
-  },
-  {
-    path: "/events/:id",
-    element: <EventDetails />,
-    children: [
-      {
-        path: "/events/:id/edit",
-        element: <EditEvent />,
-      },
-    ],
-  },
-]);
+import Places from './components/Places.jsx';
+import Modal from './components/Modal.jsx';
+import DeleteConfirmation from './components/DeleteConfirmation.jsx';
+import logoImg from './assets/logo.png';
+import AvailablePlaces from './components/AvailablePlaces.jsx';
 
 function App() {
-  return <RouterProvider router={router} />;
+  const selectedPlace = useRef();
+
+  const [userPlaces, setUserPlaces] = useState([]);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function handleStartRemovePlace(place) {
+    setModalIsOpen(true);
+    selectedPlace.current = place;
+  }
+
+  function handleStopRemovePlace() {
+    setModalIsOpen(false);
+  }
+
+  function handleSelectPlace(selectedPlace) {
+    setUserPlaces((prevPickedPlaces) => {
+      if (!prevPickedPlaces) {
+        prevPickedPlaces = [];
+      }
+      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
+        return prevPickedPlaces;
+      }
+      return [selectedPlace, ...prevPickedPlaces];
+    });
+  }
+
+  const handleRemovePlace = useCallback(async function handleRemovePlace() {
+    setUserPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
+    );
+
+    setModalIsOpen(false);
+  }, []);
+
+  return (
+    <>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText="Select the places you would like to visit below."
+          places={userPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+
+        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+      </main>
+    </>
+  );
 }
 
 export default App;
